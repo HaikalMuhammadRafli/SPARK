1afe32f7301d2fbd81e145237af08b94186e024c
unknown <fawwaz.alifio.farsa@gmail.com>
2025-06-14 23:40:12 +0700

    feat: add dosen pembimbing peran api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DosenPembimbingController;
+use App\Http\Controllers\DosenPembimbingPeranController;
 use App\Http\Controllers\KompetensiController;
 use App\Http\Controllers\MahasiswaController;
 use Illuminate\Http\Request;
@@ -18,33 +19,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum', 'authorize:admin'])->group(function () {
-    Route::prefix('mahasiswa')->group(function () {
-        Route::post('/', [MahasiswaController::class, 'store']);
-        Route::get('/', [MahasiswaController::class, 'index']);
-        Route::get('/{nim}', [MahasiswaController::class, 'show']);
-        Route::put('/{nim}', [MahasiswaController::class, 'update']);
-        Route::delete('/{nim}', [MahasiswaController::class, 'destroy']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::middleware(['authorize:admin'])->group(function () {
+        Route::prefix('mahasiswa')->group(function () {
+            Route::post('/', [MahasiswaController::class, 'store']);
+            Route::get('/', [MahasiswaController::class, 'index']);
+            Route::get('/{nim}', [MahasiswaController::class, 'show']);
+            Route::put('/{nim}', [MahasiswaController::class, 'update']);
+            Route::delete('/{nim}', [MahasiswaController::class, 'destroy']);
+        });
+        Route::prefix('admin')->group(function () {
+            Route::post('/', [AdminController::class, 'store']);
+            Route::get('/', [AdminController::class, 'index']);
+            Route::get('/{nip}', [AdminController::class, 'show']);
+            Route::put('/{nip}', [AdminController::class, 'update']);
+            Route::delete('/{nip}', [AdminController::class, 'destroy']);
+        });
+        Route::prefix('dosen-pembimbing')->group(function () {
+            Route::post('/', [DosenPembimbingController::class, 'store']);
+            Route::get('/', [DosenPembimbingController::class, 'index']);
+            Route::get('/{nip}', [DosenPembimbingController::class, 'show']);
+            Route::put('/{nip}', [DosenPembimbingController::class, 'update']);
+            Route::delete('/{nip}', [DosenPembimbingController::class, 'destroy']);
+        });
+        Route::prefix('kompetensi')->group(function () {
+            Route::post('/', [KompetensiController::class, 'store']);
+            Route::get('/', [KompetensiController::class, 'index']);
+            Route::get('/{id}', [KompetensiController::class, 'show']);
+            Route::put('/{id}', [KompetensiController::class, 'update']);
+            Route::delete('/{id}', [KompetensiController::class, 'destroy']);
+        });
     });
-    Route::prefix('admin')->group(function () {
-        Route::post('/', [AdminController::class, 'store']);
-        Route::get('/', [AdminController::class, 'index']);
-        Route::get('/{nip}', [AdminController::class, 'show']);
-        Route::put('/{nip}', [AdminController::class, 'update']);
-        Route::delete('/{nip}', [AdminController::class, 'destroy']);
-    });
-    Route::prefix('dosen-pembimbing')->group(function () {
-        Route::post('/', [DosenPembimbingController::class, 'store']);
-        Route::get('/', [DosenPembimbingController::class, 'index']);
-        Route::get('/{nip}', [DosenPembimbingController::class, 'show']);
-        Route::put('/{nip}', [DosenPembimbingController::class, 'update']);
-        Route::delete('/{nip}', [DosenPembimbingController::class, 'destroy']);
-    });
-    Route::prefix('kompetensi')->group(function () {
-        Route::post('/', [KompetensiController::class, 'store']);
-        Route::get('/', [KompetensiController::class, 'index']);
-        Route::get('/{id}', [KompetensiController::class, 'show']);
-        Route::put('/{id}', [KompetensiController::class, 'update']);
-        Route::delete('/{id}', [KompetensiController::class, 'destroy']);
+
+    Route::middleware(['authorize:dosen_pembimbing'])->group(function () {
+        Route::prefix('dosen-pembimbing-peran')->group(function () {
+            Route::post('/', [DosenPembimbingPeranController::class, 'store']);
+            Route::get('/', [DosenPembimbingPeranController::class, 'show']);
+            Route::put('/{peranId}', [DosenPembimbingPeranController::class, 'update']);
+            Route::delete('/{peranId}', [DosenPembimbingPeranController::class, 'destroy']);
+        });
     });
 });
